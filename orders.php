c27dba5074fa25aa22e641e0591b2d4dadf3bca2
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2017-09-16 17:10:42 +0300

    Update orders.php

--- orders.php
+++ orders.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="css/bootstrap.min.css" />
   <link rel="stylesheet" href="css/main.css"  />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <style>
   table {
