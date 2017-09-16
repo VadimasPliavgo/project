dbe6e61282b4ad7ef04765c3239e870f5d19dca6
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2017-09-16 18:27:56 +0300

    Update connectionDB.php

--- connectionDB.php
+++ connectionDB.php
@@ -1,8 +1,8 @@
 <?php
-$host='127.0.0.1';
-$user='root';
-$pass='';
-$Database='phoneshopdatabase';
+$host='sql11.freemysqlhosting.net';
+$user='sql11194862';
+$pass='LxX31Ngna6';
+$Database='sql11194862';
 $db = mysqli_connect($host,$user,$pass,$Database);
 if (!$db) {
   die();
