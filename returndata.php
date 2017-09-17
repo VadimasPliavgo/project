194711a29a880f070ff4bd12b258a718124b9c18
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2017-09-17 19:32:43 +0300

    Update returndata.php

--- returndata.php
+++ returndata.php
@@ -1,20 +1,11 @@
 <?php
-include 'connectionDB.php';;
+include 'connectionDB.php';
 $conn=mysqli_connect($host,$user,$pass,$Database);
 $sql= "SELECT * FROM orders";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
-   echo "<tr>";
-   echo"<td colspan='5'><b>Orders</b></td>";
-   echo"</tr>";
-   echo "<tr>";
-   echo"<td><b> ID </b></td>";
-   echo"<td><b> Name </b></td>";
-   echo"<td><b> Phone color </b></td>";
-   echo"<td><b> Phone number </b></td>";
-   echo"<td><b> Email </b></td>";
-   echo"</tr>";
+
     while($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo"<td>".$row["id"]."</td>";
