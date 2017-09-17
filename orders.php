f4ad9e4ef227d7e05917836904fe1b25491a6835
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2017-09-17 19:18:28 +0300

    Update orders.php

--- orders.php
+++ orders.php
@@ -6,6 +6,10 @@
   <link rel="stylesheet" href="css/main.css"  />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+           <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+           <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+           <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
   <script src="js/bootstrap.min.js"></script>
   <style>
   table {
@@ -18,7 +22,8 @@
 </style>
 </head>
 <body style="background-color:#1284B7;">
-    <div class="navbar navbar-invert  navbar-fixed-top" id="navbar">
+  <!--navbar-->
+  <div class="navbar navbar-invert  navbar-fixed-top" id="navbar">
    <div class="container-fluide">
      <a href="index.php" class="navbar-brand" style="width=10px;" ><b><font size="5">PhoneShop</font></b></a>
      <ul class="nav navbar-nav navbar-right">
@@ -26,10 +31,30 @@
       </ul>
     </div>
  </div>
+
+<script>
+$(document).ready(function() {
+    $('#resultable').DataTable();
+} );
+</script>
+<div class="table-responsive" id="tableclass">
+  <table  id="resultable" class="table table-striped table-bordered">
+  <thead>
+
+      <tr>
+      <th><b> ID </b></th>
+      <th><b> Name </b></th>
+      <th><b> Phone color </b></th>
+      <th><b> Phone number </b></th>
+      <th><b> Email </b></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php include 'returndata.php'; ?>
+  </tbody>
+  </table>
 </div>
-<table id="resultable" align="center" >
-  <?php include 'returndata.php'; ?>
-</table>
+
 <footer>
   <div class="bottom  bottom-fixed-bottom">
     <p>This Page is just introductory assignment to NFQ academy! It's not a real shop! Don't use your personal informacion, it might be stolen!</p>
