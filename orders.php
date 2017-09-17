c0cecd701b24eca44f40768fa5da56ac8584d9ac
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2017-09-17 19:26:42 +0300

    Update orders.php

--- orders.php
+++ orders.php
@@ -7,9 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-           <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-           <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-           <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+           <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+           <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
+           <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" />
   <script src="js/bootstrap.min.js"></script>
   <style>
   table {
