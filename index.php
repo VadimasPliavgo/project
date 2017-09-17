4b15b63936b49facde73aa4b4eb679f2ad5c1566
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2017-09-17 12:33:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <title>Phone Shop</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css" />
   <link rel="stylesheet" href="css/main.css"  />
   <meta name="viewport" content="width=device-width, initial-scale=1">
