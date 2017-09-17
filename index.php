f5a614f2df8012c91c92f9ea0efed9f911b2d1bf
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2017-09-17 14:00:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="css/bootstrap.min.css" />
   <link rel="stylesheet" href="css/main.css"  />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
 
 </head>
