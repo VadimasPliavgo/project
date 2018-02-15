0148875670e33d556c3db30fcd25d8a5f0162466
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2018-02-15 15:19:07 +0200

    Update connectionDB.php

--- connectionDB.php
+++ connectionDB.php
@@ -1,8 +1,8 @@
 <?php
-$host='sql11.freemysqlhosting.net';
-$user='sql11194862';
-$pass='LxX31Ngna6';
-$Database='sql11194862';
+$host='eu-cdbr-west-02.cleardb.net';
+$user='bb277a56596692';
+$pass='367cf596';
+$Database='heroku_041576a6f8f9aa7';
 $db = mysqli_connect($host,$user,$pass,$Database);
 if (!$db) {
   die();
