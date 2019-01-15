b30737c1ff4806c542d51154d935096abc858256
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-01-15 15:35:14 +0200

    Update Form.php

--- Test/Form.php
+++ Test/Form.php
@@ -1,1 +1,10 @@
-
+<?php
+$host='eu-cdbr-west-02.cleardb.net';
+$user='bb277a56596692';
+$pass='367cf596';
+$Database='heroku_041576a6f8f9aa7';
+$db = mysqli_connect($host,$user,$pass,$Database);
+if (!$db) {
+  die();
+}
+ ?>
