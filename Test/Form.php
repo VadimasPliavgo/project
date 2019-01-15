4fd03bbcf6b49c84f80cbf9fd99eca3421165703
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-01-15 15:38:54 +0200

    Update Form.php

--- Test/Form.php
+++ Test/Form.php
@@ -5,6 +5,7 @@ $pass='c4fe4e1d154b3a07d4592a36e0dc6afa99d0fefb1f133f1ed8b98717394ab3d6';
 $Database='d1gk77g1dd4vi9';
 $db1 = mysqli_connect($host,$user,$pass,$Database);
 if (!$db1) {
+  echo"Pizdec";
   die();
 }
 else
