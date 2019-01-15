984304e81d7519f58b28911f821386d40b96d1d7
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-01-15 15:37:44 +0200

    Update Form.php

--- Test/Form.php
+++ Test/Form.php
@@ -1,10 +1,12 @@
 <?php
-$host='eu-cdbr-west-02.cleardb.net';
-$user='bb277a56596692';
-$pass='367cf596';
-$Database='heroku_041576a6f8f9aa7';
-$db = mysqli_connect($host,$user,$pass,$Database);
-if (!$db) {
+$host='ec2-54-247-82-210.eu-west-1.compute.amazonaws.com';
+$user='oqqsodykxuvwqp';
+$pass='c4fe4e1d154b3a07d4592a36e0dc6afa99d0fefb1f133f1ed8b98717394ab3d6';
+$Database='d1gk77g1dd4vi9';
+$db1 = mysqli_connect($host,$user,$pass,$Database);
+if (!$db1) {
   die();
 }
+else
+  echo "OK";
  ?>
