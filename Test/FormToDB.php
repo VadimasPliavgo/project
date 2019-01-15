cda41e3ad8f68b5c8096843daba506c251acb190
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-01-15 23:19:09 +0200

    Update FormToDB.php

--- Test/FormToDB.php
+++ Test/FormToDB.php
@@ -10,13 +10,8 @@ $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
     or die ("Could not connect to server\n"); 
 
 $query = "SELECT * FROM salesforce.contact"; 
+echo $query;
 
-$rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
 
-while ($row = pg_fetch_row($rs)) {
-  echo "$row[0]'\n' $row[1]'\n' $row[2]'\n'";
-}
-    
-pg_close($con); 
 
 ?>
