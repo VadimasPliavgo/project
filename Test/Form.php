3e279d5fcc40249a06675cf717437c9bf15c4e8d
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-01-15 15:47:13 +0200

    Update Form.php

--- Test/Form.php
+++ Test/Form.php
@@ -15,7 +15,7 @@ else
 <?php
 $db2 = parse_url(getenv("postgres://oqqsodykxuvwqp:c4fe4e1d154b3a07d4592a36e0dc6afa99d0fefb1f133f1ed8b98717394ab3d6@ec2-54-247-82-210.eu-west-1.compute.amazonaws.com:5432/d1gk77g1dd4vi9"));
 $db2["path"] = ltrim($db["path"], "/");
-if (!$db1) {
+if (!$db2) {
   echo"Pizdec";
   die();
 }
