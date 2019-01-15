4297ecc835c3fa0c68de5ba154b9e6ff142ab1de
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-01-15 17:13:46 +0200

    Update FormToDB.php

--- Test/FormToDB.php
+++ Test/FormToDB.php
@@ -3,7 +3,7 @@
 
 $host = "ec2-54-247-82-210.eu-west-1.compute.amazonaws.com"; 
 $user = "oqqsodykxuvwqp"; 
-$pass = "c4fe4e1d154b3a07d4592a36e0dc6afa99d0fefb1f133f1ed8b98717394ab3d6*"; 
+$pass = "c4fe4e1d154b3a07d4592a36e0dc6afa99d0fefb1f133f1ed8b98717394ab3d6"; 
 $db = "d1gk77g1dd4vi9"; 
 
 $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
