7d734f15b612f562b83dc5f5c00376616ff299b8
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-01-15 16:12:57 +0200

    Update Form.php

--- Test/Form.php
+++ Test/Form.php
@@ -1,6 +1,6 @@
 <html>
   
-<form action="FormToDB.php" method="post">
+<form action="/FormToDB.php" method="post">
 Name: <input type="text" name="name"><br>
 Description: <input type="text" name="description"><br>
 <input type="submit">
