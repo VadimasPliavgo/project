01e68fe816c68c84bd99ae2ad0bed28234cc15c5
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-01-16 15:03:19 +0200

    Update FormToDB.php

--- Test/FormToDB.php
+++ Test/FormToDB.php
@@ -16,7 +16,7 @@ $sql = "INSERT INTO salesforce.contact(
 	createddate, isdeleted, name, systemmodstamp, lastname, accountid, sfid, id, _hc_lastop, _hc_err, description)
 	VALUES ($empty, $empty, $name, $empty, $empty, $empty, $empty $empty, $empty, $empty, $description)";
 
-$rs = pg_query($con, $sql) or die("Cannot execute query: $query\n");
+$rs = pg_query($con, $sql) or die("Cannot execute query: $sql\n");
 
 pg_close($con); 
 
